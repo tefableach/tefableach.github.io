34ff215e82053f6415bae615d9d58f623811f37d
Mustafa mohamed <76748956+tefableach@users.noreply.github.com>
2023-10-12 17:50:50 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,181 @@
+(function ($) {
+    "use strict";
+
+     // Spinner
+    // var spinner = function () {
+    //     setTimeout(function () {
+    //         if ($('#spinner').length > 0) {
+    //             $('#spinner').removeClass('show');
+    //         }
+    //     }, 1);
+    // };
+    // spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').css('top', '0px');
+        } else {
+            $('.sticky-top').css('top', '-100px');
+        }
+    });
+    
+    
+    // Dropdown on mouse hover
+    const $dropdown = $(".dropdown");
+    const $dropdownToggle = $(".dropdown-toggle");
+    const $dropdownMenu = $(".dropdown-menu");
+    const showClass = "show";
+    
+    $(window).on("load resize", function() {
+        if (this.matchMedia("(min-width: 992px)").matches) {
+            $dropdown.hover(
+            function() {
+                const $this = $(this);
+                $this.addClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "true");
+                $this.find($dropdownMenu).addClass(showClass);
+            },
+            function() {
+                const $this = $(this);
+                $this.removeClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "false");
+                $this.find($dropdownMenu).removeClass(showClass);
+            }
+            );
+        } else {
+            $dropdown.off("mouseenter mouseleave");
+        }
+    });
+    
+    
+    // Back to top button
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 300) {
+    //         $('.back-to-top').fadeIn('slow');
+    //     } else {
+    //         $('.back-to-top').fadeOut('slow');
+    //     }
+    // });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Date and time picker
+    $('.date').datetimepicker({
+        format: 'L'
+    });
+    $('.time').datetimepicker({
+        format: 'LT'
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 25,
+        dots: true,
+        loop: true,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+})(jQuery);
+// first inbut
+document.getElementById("popup-botton").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "flex";
+    document.querySelector(".popup2").style.display = "none";
+    document.querySelector(".popup3").style.display = "none";
+    document.querySelector(".popup4").style.display = "none";
+    document.querySelector(".popup5").style.display = "none";
+    document.querySelector(".popup6").style.display = "none";
+})
+document.querySelector(".close-window").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+})
+// secound inbut
+document.getElementById("popup-botton2").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+    document.querySelector(".popup2").style.display = "flex";
+    document.querySelector(".popup3").style.display = "none";
+    document.querySelector(".popup4").style.display = "none";
+    document.querySelector(".popup5").style.display = "none";
+    document.querySelector(".popup6").style.display = "none";
+})
+document.querySelector(".close-window2").addEventListener("click", function(){
+    document.querySelector(".popup2").style.display = "none";
+})
+// third inbut
+document.getElementById("popup-botton3").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+    document.querySelector(".popup2").style.display = "none";
+    document.querySelector(".popup3").style.display = "flex";
+    document.querySelector(".popup4").style.display = "none";
+    document.querySelector(".popup5").style.display = "none";
+    document.querySelector(".popup6").style.display = "none";
+})
+document.querySelector(".close-window3").addEventListener("click", function(){
+    document.querySelector(".popup3").style.display = "none";
+})
+// forth inbut
+document.getElementById("popup-botton4").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+    document.querySelector(".popup2").style.display = "none";
+    document.querySelector(".popup3").style.display = "none";
+    document.querySelector(".popup4").style.display = "flex";
+    document.querySelector(".popup5").style.display = "none";
+    document.querySelector(".popup6").style.display = "none";
+})
+document.querySelector(".close-window4").addEventListener("click", function(){
+    document.querySelector(".popup4").style.display = "none";
+})
+// fifth inbut
+document.getElementById("popup-botton5").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+    document.querySelector(".popup2").style.display = "none";
+    document.querySelector(".popup3").style.display = "none";
+    document.querySelector(".popup4").style.display = "none";
+    document.querySelector(".popup5").style.display = "flex";
+    document.querySelector(".popup6").style.display = "none";
+})
+document.querySelector(".close-window5").addEventListener("click", function(){
+    document.querySelector(".popup5").style.display = "none";
+})
+// sixth inbut
+document.getElementById("popup-botton6").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+    document.querySelector(".popup2").style.display = "none";
+    document.querySelector(".popup3").style.display = "none";
+    document.querySelector(".popup4").style.display = "none";
+    document.querySelector(".popup5").style.display = "none";
+    document.querySelector(".popup6").style.display = "flex";
+})
+document.querySelector(".close-window6").addEventListener("click", function(){
+    document.querySelector(".popup6").style.display = "none";
+})
